c70e305bf3b2a32ad5043f3220b197fe7e031b34
Gianluca Oldani <gianluca.oldani@unibg.it>
2023-01-30 19:15:14 +0100

    Added decks doc

--- src/deck.rs
+++ src/deck.rs
@@ -6,6 +6,7 @@ use std::sync::Mutex;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
+/// Enum that represents the status of a player's deck after an operation
 pub enum DeckStatus {
         PlayerExists,
         InsertOk
@@ -20,11 +21,15 @@ impl std::fmt::Display for DeckStatus {
         }
 }
 
+/// Type of the data used to store the association between players and card decks
 pub struct Decks {
         pub decks: HashMap<String, Vec<u8>>,
         pub last_drawn: HashMap<String, Option<u8>>,
 }
 
+/// Thread-safe wrapper around [Decks]
+///
+/// Rocket state must be thread-safe to be passed as arguments to routes functions 
 pub struct SharedDecks {
         pub data: Mutex<Decks>
 }
@@ -39,10 +44,16 @@ impl SharedDecks {
 
 impl Decks {
 
+        /// Creates a new empty set of card decks
         pub fn new() -> Decks {
                 Decks { decks: HashMap::new(), last_drawn: HashMap::new() }
         }
 
+        /// Shuffle the the deck associated with a player
+        ///
+        /// This method check that the player with name `player_name` has a deck.
+        /// If a deck is found, the last card drawn is set to `None` and the 
+        /// deck is shuffled
         pub fn shuffle_deck(&mut self, player_name: &String) {
                 let decks: &mut HashMap<String, Vec<u8>> = &mut self.decks;
                 let deck_opt = decks.get_mut(player_name);
@@ -60,14 +71,25 @@ impl Decks {
                 };
         }
 
+        /// Check if a player is associated with a deck
+        ///
+        /// This method checks if the player with name `player_name` has a deck
         pub fn has_player(&self, player_name: &String) -> bool {      
                 self.decks.contains_key(player_name)
         }
 
+        /// Retrieve the names of all the player with a deck
         pub fn get_players(&self) -> Keys<String, Vec<u8>> {
                 self.decks.keys()
         }
 
+        /// Draw a card from a player's deck
+        ///
+        /// This method check that the player with name `player_name` has a deck.
+        /// If a deck is found, a card is drawn and the last card drawn by the 
+        /// player is updated.
+        /// This method return `None` if the player's deck is empty, while `Some`
+        /// with the card values inside otherwise 
         pub fn get_card(&mut self, player_name: &String) -> Option<u8> {
                 if self.decks.contains_key(player_name) {
                         let decks: &mut HashMap<String, Vec<u8>> = &mut self.decks;
@@ -79,6 +101,10 @@ impl Decks {
                 None
         }
 
+        /// Recover the last card drawn from a player's deck
+        ///
+        /// This method return `None` if the player's deck has never drawn a card before, 
+        ///while `Some` with the card values inside otherwise 
         pub fn get_last_drawn(&self, player_name: &String) -> Option<u8> {
                 if self.last_drawn.contains_key(player_name) {
                         return *self.last_drawn.get(player_name).unwrap();
@@ -86,6 +112,7 @@ impl Decks {
                 None
         }
 
+        /// Check if the deck is empty
         pub fn is_last_card(&self, player_name: &String) -> bool {
                 if self.decks.contains_key(player_name) {
                         let decks: &HashMap<String, Vec<u8>> = &self.decks;
@@ -95,6 +122,9 @@ impl Decks {
                 false
         }
 
+        /// Inser a new deck with a player with `player_name`
+        ///
+        /// If the Decks
         pub fn insert_deck(&mut self, player_name: &String) -> DeckStatus {
                 if !self.decks.contains_key(player_name) {
                         self.decks.insert(player_name.to_string(), Vec::<u8>::new());
